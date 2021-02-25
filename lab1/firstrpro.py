b771289bf7d3ab1a1ff5de1f934da07fb1d0dcb6
kitarp07 <imlaleet@gmail.com>
2021-02-25 09:25:19 +0545

    Initial commit

---
+++ lab1/firstrpro.py
@@ -0,0 +1,7 @@
+#Write a program that takes three numbers and prints their sum. Every number is given on a separate line.
+
+a = int (input ( "Enter the number:" ))
+b = int (input ("Enter the number:" ))
+c = int (input ( "Enter the number:" ))
+d = a+b+c
+print (d)
