0a4545b1a09fd41e434abf79f6f43033c7c7458c
kitarp07 <imlaleet@gmail.com>
2021-02-25 09:48:46 +0545

    firstrpro

---
+++ lab1/printtype.py
@@ -0,0 +1,8 @@
+name = input ("Enter your name:")
+age  = int (input ("Enter your age:"))
+
+print("hello my name is" + name +  " and i am" + str(age) + " years old")
+print("hello my name is", name, "and i am", age, "years old")
+print("hello my nam is %s and i am %d years old" % (name, age))
+print("hello mt nam is {} and i am {} years old".format(name, age))
+print(f"hello my nam eis {name} and i am {age} years old")
