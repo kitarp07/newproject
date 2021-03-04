df319eed655bbac7b863f827e5b4669b40f77ded
kitarp07 <imlaleet@gmail.com>
2021-03-04 09:38:31 +0545

    midnight time

---
+++ lab1/midnight.py
@@ -0,0 +1,8 @@
+#Given the integer N - the number of minutes that is passed since midnight - how many hours and minutes
+# are displayed on the 24 h digital clock?
+
+N = int(input("Enter the minutes passed since midnight:"))
+hours = (N//60)
+minutes = (N%60)
+print(f'The hour displayed in the 24 h digital clock is {hours}')
+print(f'The minutes displayed in the 24 h digital clock is {minutes}')
