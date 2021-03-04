754a0fde110ddf05253d6150556eef88e1e86461
kitarp07 <imlaleet@gmail.com>
2021-03-04 09:54:37 +0545

    midnight time

--- lab1/midnight.py
+++ lab1/midnight.py
@@ -4,8 +4,5 @@
 N = int(input("Enter the minutes passed since midnight:"))
 hours = (N//60)
 minutes = (N%60)
-
-
-
 print(f'The hour displayed in the 24 h digital clock is {hours}')
 print(f'The minutes displayed in the 24 h digital clock is {minutes}')
